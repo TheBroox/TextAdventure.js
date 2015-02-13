2777afbf0e24ce5d13f3409dfcbf4181f605fbdd
Brooks Child <Christopher.Child@ExploreLearning.com>
2015-02-13 12:40:10 -0500

    Stubbed gameFunction Object, Stubbed Exits and Spelling Fixes

--- cartridges/Gold_Mine.js
+++ cartridges/Gold_Mine.js
@@ -1,7 +1,7 @@
 // === Game Data ===
-var cartridge = {
+var gameData = {
 	commandCounter : 0,
-	introText : 'Welcome to the Crooked Gulch Gold Mine. What it lacks in safety precautions it more than makes up for in gold. Watch your step an you might just make it out with riches beyond your wildest imagination!',
+	introText : 'Welcome to the Crooked Gulch Gold Mine. What it lacks in safety precautions it more than makes up for in gold. Watch your step and you might just make it out with riches beyond your wildest imagination!',
 	player : {
 		currentLocation : 'MineEntrance',
 		inventory : {}
@@ -19,26 +19,44 @@ var cartridge = {
 						itemCap('helmet',2);
 					}
 				},
-				sign : { look : 'The sign reads "Crooked Gulch Gold Mine" and has a note taked to the bottom of it.'},
+				sign : { look : 'The sign reads "Crooked Gulch Gold Mine" and has a note tacked to the bottom of it.'},
 				note : { look : 'Written in an untidy scroll the note reads "Generator blew. Lights out."'}
 			},
 			items : {
 				helmet : {
 					displayName : 'Miner Helmet',
-					description : 'This trusty old miner helment is covered in dents but still seems sturdy. Plus, the light works.',
+					description : 'This trusty old miner helmet is covered in dents but still seems sturdy. Plus, the light works.',
 					quantity : 1,
 					hidden : true
 				}
 			},
-			exits : {}
+			exits : {
+				north : {
+					displayName : 'North',
+					destination : 'Tunnel'
+				},
+				south : {
+					displayName : 'South',
+					destination : 'Tunnel'
+				}
+			}
 		}
 	}
 };
 
-// === External Access to Game Data ===
-module.exports.cartridge = cartridge;
-
-// === Game Unique Functions ===
-function itemCap(item, number){
-	// To come
+// === Game Functions ===
+var gameFunctions = {
+	take : function(game, command){
+		return 'Cart take';
+	}
 }
+
+
+// === Helper Functions ===
+function itemCap(item, number){
+	
+}
+
+// === External Access to Game Data ===
+module.exports.gameData = gameData;
+module.exports.gameFunctions = gameFunctions;
